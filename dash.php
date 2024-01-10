7da80778ecf51453c7fdaf8518e0649cbcd72193
R0cKy1111 <145274407+R0cKy1111@users.noreply.github.com>
2024-01-10 18:09:32 +0200

    Add files via upload

---
+++ dash.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>DASHBOARD </title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+
+<script>
+$(function () {
+    $(document).on( 'scroll', function(){
+        console.log('scroll top : ' + $(window).scrollTop());
+        if($(window).scrollTop()>=$(".logo").height())
+        {
+             $(".navbar").addClass("navbar-fixed-top");
+        }
+
+        if($(window).scrollTop()<$(".logo").height())
+        {
+             $(".navbar").removeClass("navbar-fixed-top");
+        }
+    });
+});</script>
+</head>
+
+<body  style="background:#eee;">
+<div class="header">
+<div class="row">
+<div class="col-lg-6">
+<span class="logo">Online Exam System</span></div>
+<?php
+ include_once 'dbConnection.php';
+session_start();
+$email=$_SESSION['email'];
+  if(!(isset($_SESSION['email']))){
+header("location:index.php");
+
+}
+else
+{
+$name = $_SESSION['name'];
+
+include_once 'dbConnection.php';
+echo '<span class="pull-right top title1" ><span class="log1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Hello,</span> <a href="#" class="log log1">'.$name.'</a>&nbsp;|&nbsp;<a href="logout.php?q=account.php" class="log"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Logout</button></a></span>';
+}?>
+
+</div></div>
+<!-- admin start-->
+
+<!--navigation menu-->
+<nav class="navbar navbar-default title1">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="dash.php?q=0"><b>Dashboard</b></a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="dash.php?q=0">Home<span class="sr-only">(current)</span></a></li>
+        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="dash.php?q=1">Users</a></li>
+	<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="dash.php?q=2">Exam Dates</a></li>
+    <li <?php if(@$_GET['q']==3) echo'class="active"'; ?>><a href="dash.php?q=3">Feedback</a></li>
+    <li <?php if(@$_GET['q']==4) echo'class="active"'; ?>><a href="dash.php?q=4">Add Exams</a></li>
+        
+      </ul>
+          </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!--navigation menu closed-->
+<div class="container"><!--container start-->
+<div class="row">
+<div class="col-md-12">
+<!--home start-->
+
+<?php if(@$_GET['q']==0) {
+
+$result = mysqli_query($con,"SELECT * FROM quiz ORDER BY date DESC") or die('Error');
+echo  '<div class="panel"><table class="table table-striped title1">
+<tr><td><b>S.N.</b></td><td><b>Topic</b></td><td><b>Total question</b></td><td><b>Marks</b></td><td><b>Time limit</b></td><td></td></tr>';
+$c=1;
+while($row = mysqli_fetch_array($result)) {
+	$title = $row['title'];
+	$total = $row['total'];
+	$sahi = $row['sahi'];
+    $time = $row['time'];
+	$eid = $row['eid'];
+	echo '<tr><td>'.$c++.'</td><td>'.$title.'</td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$time.'&nbsp;min</td>
+	<td><b><a href="update.php?q=rmquiz&eid='.$eid.'" class="pull-right btn btn-danger" style="margin:0px; border-radius:0%;"><span class="glyphicon glyphicon-trash"  aria-hidden="true"></span>&nbsp;<span class="title1"><b>Remove</b></span></a></b></td></tr>';
+}
+$c=0;
+echo '</table></div>';
+
+}
+
+//ranking start
+if(@$_GET['q']== 2) 
+{
+$q=mysqli_query($con,"SELECT * FROM student" )or die('Error223');
+echo  '<div class="panel title">
+<table class="table table-striped title1" >
+<tr style="color:red"><td><b></b></td><td><b>MODULE</b></td><td><b>DESCRIPTION</b></td><td><b>DATE</b></td></tr>';
+$c=0;
+while($row=mysqli_fetch_array($q) )
+{
+$module=$row['module'];
+$description=$row['description'];
+$date=$row['exam'];
+$c++;
+echo '<tr><td style="color:#99cc32"><b>'.$c.'</b></td><td>'.$module.'</td><td>'.$description.'</td><td>'.$date.'</td><td>';
+}
+echo '</table></div>';}
+?>
+
+
+<!--home closed-->
+<!--users start-->
+<?php if(@$_GET['q']==1) {
+
+$result = mysqli_query($con,"SELECT * FROM user") or die('Error');
+echo  '<div class="panel"><table class="table table-striped title1">
+<tr><td><b>S.N.</b><td><b>ID</b></td></td><td><b>Name</b></td><td><b>Email</b></td><td></td></tr>';
+$c=1;
+while($row = mysqli_fetch_array($result)) {
+$id = $row['id'];    
+$name = $row['name'];
+$email = $row['email'];
+
+	echo '<tr><td>'.$c++.'</td><td>'.$id.'</td><td>'.$name.'</td><td>'.$email.'</td>
+	<td><a title="Delete User" href="update.php?demail='.$email.'"><b><span class="glyphicon glyphicon-trash" style="color:red;" aria-hidden="true"></span></b></a></td></tr>';
+}
+$c=0;
+echo '</table></div>';
+
+}?>
+<!--user end-->
+
+<!--feedback start-->
+<?php if(@$_GET['q']==3) {
+$result = mysqli_query($con,"SELECT * FROM `feedback` ORDER BY `feedback`.`date` DESC") or die('Error');
+echo  '<div class="panel"><table class="table table-striped title1">
+<tr><td><b>S.N.</b></td><td><b>Subject</b></td><td><b>Email</b></td><td><b>Date</b></td><td><b>Time</b></td><td><b>By</b></td><td></td><td></td></tr>';
+$c=1;
+while($row = mysqli_fetch_array($result)) {
+	$date = $row['date'];
+	$date= date("d-m-Y",strtotime($date));
+	$time = $row['time'];
+	$subject = $row['subject'];
+	$name = $row['name'];
+	$email = $row['email'];
+	$id = $row['id'];
+	 echo '<tr><td>'.$c++.'</td>';
+	echo '<td><a title="Click to open feedback" href="dash.php?q=3&fid='.$id.'">'.$subject.'</a></td><td>'.$email.'</td><td>'.$date.'</td><td>'.$time.'</td><td>'.$name.'</td>
+	<td><a title="Open Feedback" href="dash.php?q=3&fid='.$id.'"><b><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span></b></a></td>';
+	echo '<td><a title="Delete Feedback" href="update.php?fdid='.$id.'"><b><span class="glyphicon glyphicon-trash" style="color:red;" aria-hidden="true"></span></b></a></td>
+
+	</tr>';
+}
+echo '</table></div>';
+}
+?>
+<!--feedback closed-->
+
+<!--feedback reading portion start-->
+<?php if(@$_GET['fid']) {
+echo '<br />';
+$id=@$_GET['fid'];
+$result = mysqli_query($con,"SELECT * FROM feedback WHERE id='$id' ") or die('Error');
+while($row = mysqli_fetch_array($result)) {
+	$name = $row['name'];
+	$subject = $row['subject'];
+	$date = $row['date'];
+	$date= date("d-m-Y",strtotime($date));
+	$time = $row['time'];
+	$feedback = $row['feedback'];
+	
+echo '<div class="panel"<a title="Back to Archive" href="update.php?q1=2"><b><span class="glyphicon glyphicon-level-up" aria-hidden="true"></span></b></a><h2 style="text-align:center; margin-top:-15px;font-family: "Ubuntu", sans-serif;"><b>'.$subject.'</b></h1>';
+ echo '<div class="mCustomScrollbar" data-mcs-theme="dark" style="margin-left:10px;margin-right:10px; max-height:450px; line-height:35px;padding:5px;"><span style="line-height:35px;padding:5px;">-&nbsp;<b>DATE:</b>&nbsp;'.$date.'</span>
+<span style="line-height:35px;padding:5px;">&nbsp;<b>Time:</b>&nbsp;'.$time.'</span><span style="line-height:35px;padding:5px;">&nbsp;<b>By:</b>&nbsp;'.$name.'</span><br />'.$feedback.'</div></div>';}
+}?>
+<!--Feedback reading portion closed-->
+
+<!--add quiz start-->
+<?php
+if(@$_GET['q']==4 && !(@$_GET['step']) ) {
+
+include_once './uploads.php';
+
+
+}
+?><!--add exam step 2 end-->
+
+<!--remove exam-->
+
+
+
+</div><!--container closed-->
+</div></div>
+</div>
+</body>
+</html>
